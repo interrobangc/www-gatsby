7cb6398f4bf0b9779655b7608b5f9235c07d2463
Robert Bo Davis <bo@interrobang.consulting>
2019-09-10 11:23:30 -0400

    fix social icon links

--- src/components/SocialIcon.js
+++ src/components/SocialIcon.js
@@ -1,10 +1,24 @@
 import React from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
+function getUrl(site, username) {
+  switch (site) {
+    case "facebook":
+      return `https://facebook.com/${username}`
+    case "github":
+      return `https://github.com/${username}`
+    case "linkedin":
+      return `https://www.linkedin.com/in/${username}`
+      break
+    default:
+      return null
+  }
+}
+
 function SocialIcon({ site, username }) {
   if (username) {
     return (
-      <a href={`http://${site}.com/${username}`}>
+      <a href={getUrl(site, username)}>
         <FontAwesomeIcon icon={["fab", site]} size="lg" />
       </a>
     )
