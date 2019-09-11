72ceb85d9013614f285adc2294c466c8a6362f5c
Robert Bo Davis <bo@interrobang.consulting>
2019-09-11 12:10:12 -0400

    Use gatsby-image for images.

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,29 +6,13 @@ module.exports = {
   },
   plugins: [
     {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `interrobang-consulting`,
-        short_name: `interrobangc`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `img/logo.png`,
-      },
-    },
-    `gatsby-plugin-offline`,
-    `gatsby-plugin-react-helmet`,
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sass`,
-    `gatsby-plugin-sharp`,
-    {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `img`,
-        path: `${__dirname}/img/`,
+        name: `images`,
+        path: `${__dirname}/images/`,
       },
     },
+    `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
     `gatsby-transformer-yaml`,
     {
@@ -37,5 +21,20 @@ module.exports = {
         path: `./content/data/`,
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `interrobang-consulting`,
+        short_name: `interrobangc`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `images/logo.png`,
+      },
+    },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sass`,
   ],
 }
--- src/subpages/Masthead.js
+++ src/subpages/Masthead.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
+import BackgroundImage from "gatsby-background-image"
 
 function Masthead() {
   const { pageDataYaml } = useStaticQuery(graphql`
@@ -7,21 +8,33 @@ function Masthead() {
       pageDataYaml {
         welcomeText
         landingText
+        headerImage {
+          childImageSharp {
+            fluid(maxWidth: 1000, maxHeight: 1500) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
       }
     }
   `)
 
-  const { welcomeText, landingText } = pageDataYaml
+  const { welcomeText, landingText, headerImage } = pageDataYaml
 
   return (
-    <header className="masthead">
+    <BackgroundImage
+      Tag="header"
+      className="masthead"
+      fluid={headerImage.childImageSharp.fluid}
+      backgroundColor={`#040e18`}
+    >
       <div className="container">
         <div className="intro-text">
           <div className="intro-lead-in">{welcomeText}</div>
           <div className="intro-heading text-uppercase">{landingText}</div>
         </div>
       </div>
-    </header>
+    </BackgroundImage>
   )
 }
 
