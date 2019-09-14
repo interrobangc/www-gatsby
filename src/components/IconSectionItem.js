4bc3b519ca3d308e79ea4f132dc052c834bfae1e
Robert Bo Davis <bo@interrobang.consulting>
2019-09-14 16:33:36 -0400

    remove react-reveal and animations

--- src/components/IconSectionItem.js
+++ src/components/IconSectionItem.js
@@ -1,18 +1,15 @@
 import React from "react"
-import Bounce from "react-reveal/Bounce"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 function IconSectionItem({ icon, title, description }) {
   return (
-    <Bounce left>
-      <div className="col-md-4">
-        <div className="iconItem">
-          <FontAwesomeIcon className="iconItemIcon" icon={icon} size="6x" />
-          <h3 className="iconItemTitle">{title}</h3>
-          <p className="iconItemDescription">{description}</p>
-        </div>
+    <div className="col-md-4">
+      <div className="iconItem">
+        <FontAwesomeIcon className="iconItemIcon" icon={icon} size="6x" />
+        <h3 className="iconItemTitle">{title}</h3>
+        <p className="iconItemDescription">{description}</p>
       </div>
-    </Bounce>
+    </div>
   )
 }
 
--- src/components/TeamItem.js
+++ src/components/TeamItem.js
@@ -1,34 +1,31 @@
 import React from "react"
-import Bounce from "react-reveal/Bounce"
 import Img from "gatsby-image"
 import SocialIcon from "./SocialIcon"
 
 function TeamItem({ name, title, image, github, linkedin, facebook }) {
   return (
-    <Bounce right>
-      <div className="col-md-4">
-        <div className="teamItem">
-          <Img
-            className="mx-auto rounded-circle teamItemImage"
-            fluid={image.childImageSharp.fluid}
-            alt={name}
-          />
-          <h4 className="mt-4">{name}</h4>
-          <p className="text-muted">{title}</p>
-          <ul className="list-inline social-buttons">
-            <li className="list-inline-item">
-              <SocialIcon site="github" username={github} />
-            </li>
-            <li className="list-inline-item">
-              <SocialIcon site="linkedin" username={linkedin} />
-            </li>
-            <li className="list-inline-item">
-              <SocialIcon site="facebook" username={facebook} />
-            </li>
-          </ul>
-        </div>
+    <div className="col-md-4">
+      <div className="teamItem">
+        <Img
+          className="mx-auto rounded-circle teamItemImage"
+          fluid={image.childImageSharp.fluid}
+          alt={name}
+        />
+        <h4 className="mt-4">{name}</h4>
+        <p className="text-muted">{title}</p>
+        <ul className="list-inline social-buttons">
+          <li className="list-inline-item">
+            <SocialIcon site="github" username={github} />
+          </li>
+          <li className="list-inline-item">
+            <SocialIcon site="linkedin" username={linkedin} />
+          </li>
+          <li className="list-inline-item">
+            <SocialIcon site="facebook" username={facebook} />
+          </li>
+        </ul>
       </div>
-    </Bounce>
+    </div>
   )
 }
 
