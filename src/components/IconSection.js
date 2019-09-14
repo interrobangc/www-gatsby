6ebc0a45d08e51c797a20de3213a530cacd81d42
Robert Bo Davis <bo@interrobang.consulting>
2019-09-14 13:29:22 -0400

    Add background image to technical services section

--- src/components/IconSection.js
+++ src/components/IconSection.js
@@ -1,14 +1,37 @@
 import React from "react"
+import BackgroundImage from "gatsby-background-image"
 import IconSectionItem from "../components/IconSectionItem"
+
+const ConditionalBackgroudImage = ({ data, children}) => {
+  console.log(data.id)
+  if (data.id && data.backgroundImage){
+    console.log('not null')
+    return (
+      <BackgroundImage
+        Tag="section"
+        id={data.id}
+        fluid={[
+          `linear-gradient(rgba(33, 37, 41, 0.80), rgba(33, 37, 41, 0.80))`,
+          data.backgroundImage.childImageSharp.fluid
+        ]}
+      >{children}</BackgroundImage>
+    )
+  } else {
+    console.log('in null')
+    return  (
+      <section id={data.id}>{children}</section>
+    )
+  }
+};
 
 function IconSection({ data }) {
   return (
-    <section id={data.id}>
+    <ConditionalBackgroudImage data={data}>
       <div className="container">
         <div className="row">
           <div className="col-lg-8 mx-auto">
             <h2 className="section-heading">{data.heading}</h2>
-            <h3 className="section-subheading text-muted">{data.subheading}</h3>
+            <h3 className="section-subheading">{data.subheading}</h3>
           </div>
         </div>
         <div className="row mx-auto">
@@ -25,7 +48,7 @@ function IconSection({ data }) {
           })}
         </div>
       </div>
-    </section>
+    </ConditionalBackgroudImage>
   )
 }
 
